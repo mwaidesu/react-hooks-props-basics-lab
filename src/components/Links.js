8413f04aaaf08af64fb6b229cd22ffd4d8ca3b52
Mwai <machariamwai@gmail.com>
2022-04-29 18:41:06 +0300

    Pass tests

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Links(props){
+  return(
+    <div>
+      <h3>Links</h3>
+      <a href={props.github} target="_blank" rel="noopener noreferrer">{props.github}</a>
+      <a href={props.linkedin} target="_blank" rel="noopener noreferrer">{props.linkedin}</a>
+    </div>
+  )
+}
+
+export default Links;
